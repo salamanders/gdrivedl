1cbd90261ff664614f9cd09673110e6a238a4bee
Benjamin Hill <benjaminhill@gmail.com>
2020-03-11 10:15:00 -0700

    download all non-native files from root

--- src/main/kotlin/info/benjaminhill/gdrivedl/Download.kt
+++ src/main/kotlin/info/benjaminhill/gdrivedl/Download.kt
@@ -1,14 +1,15 @@
 package info.benjaminhill.gdrivedl
 
+import com.google.api.services.drive.model.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
 
 fun main() {
-    processDir("1NOrBLUi-qyUjM1C4wangO69O-K9OyFoS", Paths.get("./downloads"))
+    processDir("root", Paths.get("./downloads"))
 }
 
-fun processDir(remoteDirId: String, localDir: Path) {
+private fun processDir(remoteDirId: String, localDir: Path) {
     println(localDir)
     var pageToken: String? = null
     do {
@@ -20,31 +21,30 @@ fun processDir(remoteDirId: String, localDir: Path) {
             .execute()
 
         result.files?.filterNotNull()?.forEach { file ->
-            when {
-                // recurse (don't create dirs yet)
-                file.mimeType == "application/vnd.google-apps.folder" -> {
-                    processDir(file.id, localDir.resolve(file.name.replace("\\s+", "_")))
-                }
-                file.mimeType.startsWith("application/vnd.google-apps") -> {
-                    println("Skipping native file '${file.name}'")
-                }
-                else -> {
-                    println("Downloading ${file.mimeType} '${file.name}' (${file.quotaBytesUsed})")
-                    localDir.toFile().mkdirs()
-                    localDir.resolve(file.name.replace("\\s+", "_")).toFile().outputStream().use { outputStream ->
-                        service.files().get(file.id)
-                            .executeMediaAndDownloadTo(outputStream)
-                    }
-                }
-            }
+            processFile(file, localDir)
         }
 
         pageToken = result.nextPageToken
     } while (pageToken != null)
 }
 
+private fun processFile(file: File, localDir: Path) {
+    when {
+        // recurse (don't create dirs yet)
+        file.mimeType == "application/vnd.google-apps.folder" -> {
+            processDir(file.id, localDir.resolve(file.name.replace("\\s+", "_")))
+        }
+        file.mimeType.startsWith("application/vnd.google-apps") -> {
+            println("Skipping native file '${file.name}'")
+        }
+        else -> {
+            println("Downloading ${file.mimeType} '${file.name}' (${file.quotaBytesUsed})")
+            localDir.toFile().mkdirs()
+            localDir.resolve(file.name.replace("\\s+", "_")).toFile().outputStream().use { outputStream ->
+                service.files().get(file.id)
+                    .executeMediaAndDownloadTo(outputStream)
+            }
+        }
+    }
+}
 
-/*
-val outputStream: OutputStream = ByteArrayOutputStream()
-
-*/
